4fe1a41673c0aa25abfa150b4247e4c9ffca725b
ShionKigami <steblewets.ant@gmail.com>
2026-06-01 13:13:04 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Очищаем все данные сессии
 $_SESSION = array();
 
-// Удаляем cookie сессии
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -13,10 +11,8 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Уничтожаем сессию
 session_destroy();
 
-// Перенаправляем на главную страницу с формой
 header('Location: index.php');
 exit();
 ?>
